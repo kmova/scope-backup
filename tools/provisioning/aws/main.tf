32f625b7b9b11544c3d39053bc52906f4f7f57d0
Akash Srivastava <akashsrivastava4927@gmail.com>
2025-07-24 22:24:17 +0530

    Automated remediation of security findings

--- tools/provisioning/aws/main.tf
+++ tools/provisioning/aws/main.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["${var.client_ip}/32"]
   }
 
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags {
     Name      = "${var.name}_allow_ssh"
     App       = "${var.app}"
@@ -80,23 +87,14 @@ resource "aws_security_group" "allow_private_ingress" {
   }
 }
 
-resource "aws_security_group" "allow_all_egress" {
-  name        = "${var.name}_allow_all_egress"
-  description = "AWS security group to allow all egress traffic on AWS EC2 instances (created using Terraform)."
+resource "aws_instance" "tf_test_vm" {
+  ...
 
-  # Full outbound internet access on both TCP and UDP
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  metadata_options {
+    http_tokens = "required"
   }
 
-  tags {
-    Name      = "${var.name}_allow_all_egress"
-    App       = "${var.app}"
-    CreatedBy = "terraform"
-  }
+  ...
 }
 
 resource "aws_instance" "tf_test_vm" {
